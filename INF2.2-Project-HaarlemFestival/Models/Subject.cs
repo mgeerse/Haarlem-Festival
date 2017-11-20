5fded23affc8e94f4332e2157545a5d0af304197
Maarten Geerse <582297@student.inholland.nl>
2017-11-20 15:15:47 +0100

    SubjectModel + constructor

---
+++ INF2.2-Project-HaarlemFestival/Models/Subject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INF2._2_Project_HaarlemFestival.Models
+{
+    public class Subject
+    {
+        public int SubjectId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        //Constructor
+        public Subject(int Id, string Name, string Description)
+        {
+            SubjectId = Id;
+            this.Name = Name;
+            this.Description = Name;
+        }
+    }
+}
