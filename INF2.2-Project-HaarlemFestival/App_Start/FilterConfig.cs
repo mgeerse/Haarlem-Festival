f693207ebfd34e132ff7efb35751a64106541626
Maarten Geerse <582297@student.inholland.nl>
2017-11-20 15:10:03 +0100

    Add project files.

---
+++ INF2.2-Project-HaarlemFestival/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace INF2._2_Project_HaarlemFestival
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ INF2.2-Project-HaarlemFestival/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="INF2._2_Project_HaarlemFestival.MvcApplication" Language="C#" %>
