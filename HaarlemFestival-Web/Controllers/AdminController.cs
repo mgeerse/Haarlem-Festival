702e369189e85d41df733bdd2af6b088343c187f
Teije <teije@tptuned.com>
2017-12-06 14:26:02 +0100

    Lege pagina aangemaakt

---
+++ HaarlemFestival-Web/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HaarlemFestival_Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
