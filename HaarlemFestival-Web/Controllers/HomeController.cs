cde1937ab9c09f48aab9daed9c35988d5e7e7c94
Maarten Geerse <582297@student.inholland.nl>
2017-12-19 14:46:13 +0100

    Added initial code

--- HaarlemFestival-Web/Controllers/HomeController.cs
+++ HaarlemFestival-Web/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HaarlemFestival_Web.Contexts;
 
 namespace HaarlemFestival_Web.Controllers
 {
     public class HomeController : Controller
     {
+        private FestivalContext FestivalContext = new FestivalContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(FestivalContext.Subjects.ToList());
         }
     }
 }
