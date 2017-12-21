f75738931f7a82a045ff88e27e467c727b94a0cf
Teije <teije@tptuned.com>
2017-12-21 14:33:22 +0100

    Stash

--- HaarlemFestival-Web/Controllers/AdminController.cs
+++ HaarlemFestival-Web/Controllers/AdminController.cs
@@ -3,21 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HaarlemFestival_Web.Models;
+using HaarlemFestival_Web.Contexts;
 
 namespace HaarlemFestival_Web.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
-        public ActionResult Index()
+        FestivalContext db = new FestivalContext();
+
+        //:Registration
+        [HttpGet]
+        public ActionResult Registration()
         {
             return View();
         }
 
-        // GET: Login
-        public ActionResult Login()
+        //:Registration [POST]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration(Account a)
         {
-            return View();
+            bool status = false;
+            string message = "";
+
+            if(ModelState.IsValid)
+            {
+                //-Username Exist Check
+                if(UsernameExists(a.Username))
+                {
+                    ModelState.AddModelError("UsernameExists", "Username already exists");
+                }
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+            //-Process form data and write to database
+
+            //-Model validation
+
+            //-Password Hashing
+
+            //-Store data to database
+
+
+
+            return View(a);
+        }
+
+        //:Login
+
+
+        //:Login [POST]
+
+
+        //: Logout
+
+
+        [NonAction]
+        public bool UsernameExists(string enteredUsername)
+        {
+            var existingUsername = db.Accounts.Where(u => u.Username == enteredUsername).FirstOrDefault();
+
+            return existingUsername == null ? false : true;
+
         }
     }
 }
