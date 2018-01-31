447b3194c1e1ee8ea1a70a345505078720934c68
Teije <teije@tptuned.com>
2018-01-31 14:44:55 +0100

    AccountTableMerge

--- HaarlemFestival-Web/Models/Account.cs
+++ HaarlemFestival-Web/Models/Account.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -11,11 +12,22 @@ namespace HaarlemFestival_Web.Models
         [Key]
         public int Id { get; private set; }
 
-        // Non-referencing properties
+        [Required(ErrorMessage = "The 'Name' field cannot be empty.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "The 'Username' field cannot be empty.")]
         public string Username { get; set; }
+
         // TODO: Encrypt passwords. Extra points.
+        [Required(ErrorMessage = "The 'Password' field cannot be empty.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        //[NotMapped]
+        //[Compare("Password", ErrorMessage = "Passwords must be equal.")]
+        //[DataType(DataType.Password)]
+        //public string ConfirmPassword { get; set; }
+
         public AccountType AccountType { get; set; }
     }
 }
