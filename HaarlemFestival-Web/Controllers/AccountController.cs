59946adc998da2e2db0df3ef3b1385896ceb1904
Teije <teije@tptuned.com>
2018-01-31 16:46:29 +0100

    FixNuGet

--- HaarlemFestival-Web/Controllers/AccountController.cs
+++ HaarlemFestival-Web/Controllers/AccountController.cs
@@ -19,11 +19,11 @@ namespace HaarlemFestival_Web.Controllers
         }
 
         #region Registration
-        [HttpGet]
-        public ActionResult Register()
-        {
-            return View();
-        }
+            [HttpGet]
+            public ActionResult Register()
+            {
+                return View();
+            }
 
         [NonAction]
         public bool UsernameExists(string enteredUsername)
@@ -34,73 +34,73 @@ namespace HaarlemFestival_Web.Controllers
 
         }
 
-        [HttpPost]
-        public ActionResult Register(Account Account)
-        {
-            if(ModelState.IsValid)
+            [HttpPost]
+            public ActionResult Register(Account Account)
             {
-                using (FestivalContext Db = new FestivalContext())
+                if(ModelState.IsValid)
                 {
-                    Db.Accounts.Add(Account);
-                    Db.SaveChanges();
-                }
+                    using (FestivalContext Db = new FestivalContext())
+                    {
+                        Db.Accounts.Add(Account);
+                        Db.SaveChanges();
+                    }
 
-                ModelState.Clear();
-                ViewBag.Message = Account.Name + ", your account has been successfully registered!";
+                    ModelState.Clear();
+                    ViewBag.Message = Account.Name + ", your account has been successfully registered!";
+                }
+                return View();
             }
-            return View();
-        }
         #endregion
 
         #region login
-        [HttpGet]
-        public ActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Login(Account Account)
-        {
-            using (FestivalContext Db = new FestivalContext())
+            [HttpGet]
+            public ActionResult Login()
             {
-                Account user = Db.Accounts.Single(u => u.Username == Account.Username && u.Password == Account.Password);
+                return View();
+            }
 
-                if(user != null)
+            [HttpPost]
+            public ActionResult Login(Account Account)
+            {
+                using (FestivalContext Db = new FestivalContext())
                 {
-                    Session["Id"] = user.Id;
-                    Session["Username"] = user.Username;
+                    Account user = Db.Accounts.Single(u => u.Username == Account.Username && u.Password == Account.Password);
 
+                    if(user != null)
+                    {
+                        Session["Id"] = user.Id;
+                        Session["Username"] = user.Username;
+
+                        return View("LoggedIn");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Username or password is not correct");
+                    }
+                }
+
+                return View("Login");
+            }
+
+            public ActionResult LoggedIn()
+            {
+                if(Session["Id"] != null)
+                {
                     return View("LoggedIn");
                 }
                 else
                 {
-                    ModelState.AddModelError("", "Username or password is not correct");
+                    return RedirectToAction("Login");
                 }
             }
-
-            return View("Login");
-        }
-
-        public ActionResult LoggedIn()
-        {
-            if(Session["Id"] != null)
-            {
-                return View("LoggedIn");
-            }
-            else
-            {
-                return RedirectToAction("Login");
-            }
-        }
         #endregion
 
         #region Logout
-        public ActionResult Logout(Account User)
-        {
-            Session.Clear();
-            return RedirectToAction("Login");
-        }
+            public ActionResult Logout(Account User)
+            {
+                Session.Clear();
+                return RedirectToAction("Login");
+            }
         #endregion
     }
 }
