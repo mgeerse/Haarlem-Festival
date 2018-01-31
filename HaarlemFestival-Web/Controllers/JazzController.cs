5f378a5bfb073f14766b5405472e41fdc2f9c6e8
Maarten Geerse <mgeerse@gmail.com>
2018-01-31 15:34:50 +0100

    Tussentijdje commit

---
+++ HaarlemFestival-Web/Controllers/JazzController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HaarlemFestival_Web.Repositories;
+using HaarlemFestival_Web.Models;
+
+namespace HaarlemFestival_Web.Controllers
+{ 
+    public class JazzController : Controller
+    {
+        JazzRepository JR = new JazzRepository();
+        
+        // GET: Jazz
+        public ActionResult Information(string Day)
+        {
+            List<Jazz> list = JR.GetByDay(Day);
+
+            if (list != null)
+            {
+                return View(list);
+            }
+            else
+            {
+                return View("~/Views/Pages/Jazz");
+            }
+        }
+    }
+}
